70736b936ec5e96443ab5082ed4b70449466d071
dbusrepo <dbusrepo@gmail.com>
2025-04-25 12:34:37 +0200

    chore: remove camera module not used for now

--- src/app/engine/world/mod.rs
+++ src/app/engine/world/mod.rs
@@ -1,21 +1,13 @@
 use anyhow::Result;
 
-mod camera;
-
-use camera::Camera;
-
-pub(super) struct World {
-    camera: Camera,
-}
+pub(super) struct World {}
 
 impl World {
     pub(super) fn new() -> Result<Self> {
-        Ok(Self { camera: Camera::new() })
+        Ok(Self {})
     }
 
     pub(super) fn update(&mut self, step_time: f64) -> Result<()> {
-        self.camera.update(step_time);
-
         Ok(())
     }
 }
--- src/app/mod.rs
+++ src/app/mod.rs
@@ -3,7 +3,6 @@ use std::rc::Rc;
 use std::{thread, time};
 
 use anyhow::{Result, anyhow};
-use egui_render::EguiRender;
 use egui_sdl2_platform::sdl2::EventPump;
 use egui_sdl2_platform::{Platform, sdl2};
 use enum_map::{Enum, EnumMap, enum_map};
@@ -23,6 +22,7 @@ mod screen_quad;
 mod sdl_wgpu;
 mod terminal;
 
+use egui_render::EguiRender;
 use engine::{Engine, EngineConfiguration};
 use frame_history::FrameHistory;
 use gui::Gui;
